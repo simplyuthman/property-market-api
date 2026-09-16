566a076b08e1ad0f17923754d1640c20eabbdf7f
uthman umar <deen.duniya.developer@gmail.com>
2026-09-16 13:07:55 +0100

    feat: add prisma configuration file

--- apps/api/prisma.config.ts
+++ apps/api/prisma.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from '@prisma/config';
+import "dotenv/config";
+import { defineConfig } from "@prisma/config";
 
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
+  schema: "prisma/schema.prisma",
 });
